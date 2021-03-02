0b6f574bab2c71f36f561227906a9ef922d3412a
Admin <Admin@DESKTOP-VPF0AQ3>
2021-03-02 22:26:35 +0700

    Lập trình khai phá luật kết hợp

--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -29,7 +29,7 @@ public class WekaPro {
 
        MyFPGrowthModel model = new MyFPGrowthModel("C:\\\\Program Files\\\\Weka-3-8-5\\\\data\\\\weather.nominal.arff",
                                                      "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
-                                                     "-N -R first-last");
+                                                     "-N-R first-last");
        model.mineAssociationRules();
        System.out.println(model);
     }
